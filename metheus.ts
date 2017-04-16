950d668760adfa6b1050345ee6f8134fac968027
Mike Smullin <mike@smullindesign.com>
2017-04-15 22:06:38 -0600

    pivoting to get optimizations

--- metheus.ts
+++ metheus.ts
@@ -95,43 +95,78 @@ class Solver {
 			Log.out(`You win in ${this.guesses} guesses.`);
 			return;
 		}
-		
+
+		let twoMaybes = false;
 		// try again
-		if (null != this.board.getMove(-1)) {
-			if (correct == this.lastCorrect) {
+		let previous = this.board.getMove(-1);
+		if (null != previous) {
+			if (delta == 0) {
 				// no difference; both are wrong
 				this.board.setDeduction("NO", "NO");
+
+				// TRICKY: if previous swap was MAYBE, MAYBE deduce remaining MAYBE => YES
+				this.board.setRemainderDeduction("MAYBE", "MAYBE", "YES");
 			}
-			else if (correct == this.lastCorrect + 1) {
-				// difference; one is now right, but we don't know which
-				// unless we have history to narrow it, then we have specific match
-				this.board.setDeduction("MAYBE", "MAYBE");
+			else if (delta == 1) {
+				// if either of these MAYBEs has ever been NO before, then the other is YES now
+				if (this.board.hadDeductionAtIndex(previous.after.a, "NO")) {
+					this.board.getSlot(previous.after.a.index).setDeduction("YES");
+					this.board.getSlot(previous.after.b.index).setDeduction("NO");
+				}
+				else if (this.board.hadDeductionAtIndex(previous.after.b, "NO")) {
+					this.board.getSlot(previous.after.b.index).setDeduction("YES");
+					this.board.getSlot(previous.after.a.index).setDeduction("NO");
+				}
+				else {
+					// difference; one is now right, but we don't know which
+					// unless we have history to narrow it, then we have specific match
+					this.board.setDeduction("MAYBE", "MAYBE");
+					twoMaybes = true;
+				}
 			}
-			else if (correct == this.lastCorrect + 2) {
+			else if (delta == 2) {
 				// very positive difference; both are now right for sure
 				this.board.setDeduction("YES", "YES");
+
+				// if last two were also maybes, then the third is YES
 			}
-			else if (correct == this.lastCorrect - 1) {
+			else if (delta == -1) {
 				// difference; one was right, but we don't know which
 
-				// if one was a maybe and one was not, then one is now a for sure
+				// TRICKY: if swap MAYBE, NO = -1 then REVERSE and deduce MAYBE => YES
+
+				// if previously MAYBE, MAYBE, but now -1, then a is YES
+				if (previous.after.a.deduction == "MAYBE" && previous.after.b.deduction == "MAYBE")
+				{
+					this.board.getSlot(previous.after.a.index)
+				}
 
 				this.board.unswap();
 				correct = this.lastCorrect;
 				this.board.setDeduction("MAYBE", "MAYBE");
+				twoMaybes = true;
+
+				this.board.setRemainderDeduction("MAYBE", "MAYBE", "YES");
 			}
-			else if (correct == this.lastCorrect - 2) {
+			else if (delta == -2) {
 				// difference; both were right for sure
 				this.board.unswap();
 				correct = this.lastCorrect;
 				this.board.setDeduction("YES", "YES");
+				// if last two were also maybes, then the third is YES
 			}
 		}
 		// swap next pair
 		let candidates: number[] = [];
+		let priorityCandidates = [];
+		if (twoMaybes) {
+			// a MAYBE, MAYBE result should always be followed by 
+			// a swap containing a) one of the original maybes, and b) one new maybe
+			priorityCandidates.push(this.board.getMove(-1).after.a.index);
+		}
 		for (let i = 0, len = this.puzzle.getSlotCount(); i < len; i++) {
 			//console.log("candidate ", { i: i, d: this.board.getSlot(i).deduction });
-			if ("YES" != this.board.getSlot(i).deduction) {
+			if ("YES" != this.board.getSlot(i).deduction && priorityCandidates[0] != i) {
 				candidates.push(i);
 			}
 		}
@@ -140,7 +175,7 @@ class Solver {
 			console.log(candidates);
 			return;
 		}
-		candidates = _.shuffle(candidates);
+		candidates = _.shuffle(candidates).concat(priorityCandidates);
 		this.board.swap(candidates.pop(), candidates.pop());
 
 		this.lastCorrect = correct;
@@ -155,9 +190,17 @@ class Solver {
 type Deduction = "UNKNOWN" | "NO" | "MAYBE" | "YES";
 
 class Slot {
+	index: number;
 	symbol: string;
 	quantity: number;
 	deduction: Deduction;
+	remainder: Slot;
+
+	public setDeduction(deduction: Deduction)
+	{
+		this.deduction = deduction;
+		Log.out(`          ${this.quantity} ${this.deduction[0]} => ${deduction[0]}`);
+	}
 
 	public toString() {
 		return this.quantity;
@@ -167,6 +210,10 @@ class Slot {
 class Pair {
 	public a: Slot;
 	public b: Slot;
+
+	public matchesDeductions(a: Deduction, b: Deduction): boolean {
+		return this.a.deduction == a && this.b.deduction == b;
+	}
 }
 
 class Move {
@@ -182,6 +229,7 @@ class Board {
 	public constructor(quantities: number[]) {
 		for (let i = 0, len = quantities.length; i < len; i++) {
 			let slot = new Slot();
+			slot.index = i;
 			slot.symbol = Board.symbols.substr(i, 1);
 			slot.quantity = quantities[i];
 			slot.deduction = "UNKNOWN";
@@ -198,42 +246,56 @@ class Board {
 		move.before = pairBefore;
 
 		// move
-		Log.out(`        m: ${this.board[a].quantity} <> ${this.board[b].quantity}`);
+		Log.out(`          ${this.board[a].quantity} <> ${this.board[b].quantity}`);
 		let c = this.board[a];
 		this.board[a] = this.board[b];
 		this.board[b] = c;
+		this.board[a].index = b;
+		this.board[b].index = a;
 
 		// record after
 		let pairAfter = new Pair();
-		pairAfter.a = _.clone(this.board[a]);
-		pairAfter.b = _.clone(this.board[b]);
+		pairAfter.a = this.board[a];
+		pairAfter.b = this.board[b];
 		move.after = pairAfter;
 		this.history.push(move);
 	}
 
-	public setDeduction(a: Deduction, b: Deduction) {
-		let lastPair = this.history[this.history.length - 1].after;
-		Log.out(`        d: ${lastPair.a.quantity} ${lastPair.a.deduction[0]} => ${a[0]}, ${lastPair.b.quantity} ${lastPair.b.deduction[0]} => ${b[0]}`);
-		let ai = this.board.indexOf(lastPair.a);
-		lastPair.a.deduction = this.board[this.findSlotIndex(lastPair.a)].deduction = a;
-		lastPair.b.deduction = this.board[this.findSlotIndex(lastPair.b)].deduction = b;
-	}
-
-	public findSlotIndex(slot: Slot): number
-	{
-		for (let i=0,len=this.board.length; i<len; i++)
-		{
-			if (slot.quantity == this.board[i].quantity)
-			{
-				return i;
+	// determine whether a quantity has ever had a given deduction at a specific index in entire history
+	public hadDeductionAtIndex(slot: Slot, deduction: Deduction): boolean {
+		for (let i=0,len=this.history.length; i<len; i++) {
+			let pair = this.history[i].after;
+			if (
+				(pair.a.quantity == slot.quantity && 
+				 pair.a.deduction == deduction && 
+				 pair.a.index == slot.index) ||
+				(pair.b.quantity == slot.quantity && 
+				 pair.b.deduction == deduction && 
+				 pair.b.index == slot.index)
+			) {
+				return true;
 			}
 		}
-		throw "Quantity not found on board? Impossible!";
+		return false;
+	}
+
+	public setRemainderDeduction(a: Deduction, b: Deduction, c: Deduction) {
+		let previous = this.getMove(-1);
+		if (previous.before.matchesDeductions(a, b)) {
+			let slot = this.getSlot(previous.before.a.index);
+			slot.setDeduction(c);
+		}
+	}
+
+	public setDeduction(a: Deduction, b: Deduction) {
+		let lastPair = this.history[this.history.length - 1].after;
+		lastPair.a.setDeduction(a);
+		lastPair.b.setDeduction(b);
 	}
 
 	public unswap() {
 		let lastPair = this.history[this.history.length - 1].after;
-		this.swap(this.findSlotIndex(lastPair.a), this.findSlotIndex(lastPair.b));
+		this.swap(lastPair.a.index, lastPair.b.index);
 		Log.out(`        ${this}`);
 	}
 
@@ -252,7 +314,7 @@ class Board {
 	public render() {
 		for (let i = 0, len = this.board.length; i < len; i++) {
 			let slot = this.getSlot(i);
-			$("td#c" + i).text(slot.quantity +" "+ slot.deduction[0]);
+			$("td#c" + i).text(slot.quantity + " " + slot.deduction[0]);
 		}
 	}
 }
@@ -260,6 +322,7 @@ class Board {
 
 const GUESS_DELAY = 250; // ms
 const SLOT_COUNT = 6;
-let puzzle = PuzzleDemo.random(SLOT_COUNT);
+//let puzzle = PuzzleDemo.random(SLOT_COUNT);
+let puzzle = new PuzzleDemo([2, 3, 4, 1, 6, 5]);
 //let human = new HumanPuzzleInterface(SLOT_COUNT);
 Solver.solve(puzzle);
