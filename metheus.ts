1044a54e694e3c09529c26bd15fdc4a15d03503f
Mike Smullin <mike@smullindesign.com>
2017-04-16 14:05:35 -0600

    looking good in bst so far

--- metheus.ts
+++ metheus.ts
@@ -73,43 +73,94 @@ const NO = "NO";
 const MAYBE = "MAYBE";
 const YES = "YES";
 
+class Swap {
+	public a: number;
+	public b: number;
+
+	public constructor(a: number, b: number) {
+		this.a = a;
+		this.b = b;
+	}
+}
+
 class Move {
 	public num: number;
-	public prev?: Move;
 	public board: Board;
-	public pair?: Pair;
-	public correct: number;
-	public delta: number;
+	public correct: number; // score
+	public delta: number; // score
+	public prev?: Move; // play sequence
+	public swap?: Swap;
+	private left: Move; // bst
+	private right: Move; // bst
 
-	// new move based on given board
-	public constructor(board: Board, aIndex?: number, bIndex?: number) {
-		if (null != aIndex && null != bIndex) {
-			let oldA = _.cloneDeep(board.slots[aIndex]);
-			let oldB = _.cloneDeep(board.slots[bIndex]);
-			board.slots[aIndex] = oldB;
-			board.slots[aIndex].index = aIndex;
-			board.slots[bIndex] = oldA;
-			board.slots[bIndex].index = bIndex;
+	// new move based on copy of given board
+	public constructor(board: Board, swap?: Swap) {
+		this.board = _.cloneDeep(board);
+		if (null != swap) {
+			let tmp = this.board.slots[swap.a];
+			this.board.slots[swap.a] = this.board.slots[swap.b];
+			this.board.slots[swap.b] = tmp;
+			this.board.slots[swap.a].index = swap.a;
+			this.board.slots[swap.b].index = swap.b;
+			this.swap = swap;
 		}
-		move.board = _.cloneDeep(board);
-		if (null != aIndex && null != bIndex) {
-			move.pair = new Pair();
-			move.pair.a = _.cloneDeep(board.slots[aIndex]);
-			move.pair.b = _.cloneDeep(board.slots[bIndex]);
-		}
-		return move;
 	}
 
 	public render() {
 		let s = "";
 		for (let i = 0, len = this.board.slots.length; i < len; i++) {
 			let slot = this.board.slots[i];
-			let underline = (null != this.pair && (slot.quantity == this.pair.a.quantity || slot.quantity == this.pair.b.quantity));
+			let underline = (null != this.swap && (slot.quantity == this.board.slots[this.swap.a].quantity || slot.quantity == this.board.slots[this.swap.b].quantity));
 			let bold = this.board.debugViewCorrectSlots[i];
 			s += `${bold ? "<b>" : ""}${underline ? "<u>" : ""}${slot.quantity}${slot.deduction[0]}${underline ? "</u>" : ""}${bold ? "</b>" : ""} `;
 		}
 		s += ` = ${this.correct} (${this.delta < 0 ? this.delta : `+${this.delta}`})`;
 		Log.html(s);
+	}
+
+	public score() {
+		return this.correct + this.delta;
+	}
+
+	public static rankInsert(root: Move, move: Move) {
+		let parent: Move = null;
+		let current = move;
+		while (true) {
+			parent = current;
+			if (move.score() < parent.score()) {
+				current = parent.left;
+				if (null == current) {
+					parent.left = move;
+					return;
+				}
+			}
+			else {
+				current = parent.right;
+				if (null == current) {
+					parent.right = move;
+					return;
+				}
+			}
+		}
+	}
+
+	public static findMostValuableMove(root: Move): Move {
+		if (null == root) return null;
+		let current = root;
+		let lastHighScore = current;
+		while (true) {
+			if (current.score() > lastHighScore.score())
+			{
+				current = current.left;
+			}
+			else
+			{
+				current = current.right;
+			}
+			if (null == current) {
+				return lastHighScore;
+			}
+		}
 	}
 }
 
@@ -128,14 +179,6 @@ class Slot {
 	}
 }
 
-class Pair {
-	public a: Slot;
-	public b: Slot;
-
-	public matchesDeductions(a: Deduction, b: Deduction): boolean {
-		return this.a.deduction == a && this.b.deduction == b;
-	}
-}
 
 class Board {
 	public slots: Slot[];
@@ -174,13 +217,14 @@ class Board {
 class Solver {
 	private puzzle: Puzzle;
 	private moves: number = 0;
-	public lastMove: Move; // history of moves
+	public rootMove: Move; // root BST node
+	public lastMove: Move; // history of played moves
 
 	public constructor(puzzle: Puzzle) {
 		this.puzzle = puzzle;
 
-		// play opening move
-		this.playMove(this.newMove(new Board(_.map(
+		// begin with opening move
+		this.rootMove = new Move(new Board(_.map(
 			new Array(this.puzzle.getSlotCount()),
 			(nil, i) => {
 				let slot = new Slot();
@@ -188,7 +232,8 @@ class Solver {
 				slot.quantity = i + 1;
 				slot.deduction = UNKNOWN;
 				return slot;
-			}))));
+			})));
+		this.playMove(this.rootMove);
 	}
 
 	public playMove(move: Move) {
@@ -197,54 +242,57 @@ class Solver {
 		move.prev = this.lastMove;
 		this.lastMove = move;
 
+		// test and score the move
 		if (this.moves > MAX_GUESSES) {
 			Log.out("Too many guesses; we lose.");
 			return;
 		}
-
-		{
-			let correct = this.puzzle.test(this.lastMove.board);
-			let delta = correct - (null != this.lastMove ? this.lastMove.correct || 0 : 0);
-			this.lastMove.board.render();
-			this.lastMove.correct = correct;
-			this.lastMove.delta = delta;
-			this.lastMove.render();
+		else {
+			let correct = this.puzzle.test(move.board);
+			let delta = correct - (null != move ? move.correct || 0 : 0);
+			Move.rankInsert(this.rootMove, move);
+			move.board.render();
+			move.correct = correct;
+			move.delta = delta;
+			move.render();
 			if (correct == this.puzzle.getSlotCount()) {
 				Log.out(`You win in ${this.moves} moves.`);
 				return;
 			}
 		}
 
-		// try again
-		if (null != this.lastMove.pair) {
-			if (this.lastMove.delta == 0) {
+		// decide what to do next
+
+
+		if (null != move.swap) {
+			if (move.delta == 0) {
 				// no difference; both must be NO?
-				this.lastMove.pair.a.setDeduction(NO);
-				this.lastMove.pair.b.setDeduction(NO);
+				move.swap.a.setDeduction(NO);
+				move.swap.b.setDeduction(NO);
 			}
-			else if (this.lastMove.delta == 1) {
+			else if (move.delta == 1) {
 				// difference; one is now right, but we don't know which
 				// unless we have history to narrow it, then we have specific match
-				this.lastMove.pair.a.setDeduction(MAYBE);
-				this.lastMove.pair.b.setDeduction(MAYBE);
+				move.swap.a.setDeduction(MAYBE);
+				move.swap.b.setDeduction(MAYBE);
 			}
-			else if (this.lastMove.delta == 2) {
+			else if (move.delta == 2) {
 				// very positive difference; both are now right for sure
-				this.lastMove.pair.a.setDeduction(YES);
-				this.lastMove.pair.b.setDeduction(YES);
+				move.swap.a.setDeduction(YES);
+				move.swap.b.setDeduction(YES);
 			}
-			else if (this.lastMove.delta == -1) {
+			else if (move.delta == -1) {
 				// difference; one was right, but we don't know which
 				// so first step is always to put it back
-				this.lastMove.pair.a.setDeduction(MAYBE);
-				this.lastMove.pair.b.setDeduction(MAYBE);
-				this.unswap();
+				move.swap.a.setDeduction(MAYBE);
+				move.swap.b.setDeduction(MAYBE);
+				this.rollback();
 			}
-			else if (this.lastMove.delta == -2) {
+			else if (move.delta == -2) {
 				// difference; both were right for sure
-				this.lastMove.pair.a.setDeduction(YES);
-				this.lastMove.pair.b.setDeduction(YES);
-				this.unswap();
+				move.swap.a.setDeduction(YES);
+				move.swap.b.setDeduction(YES);
+				this.rollback();
 			}
 		}
 
@@ -253,8 +301,8 @@ class Solver {
 
 	// determine whether a quantity has ever had a given deduction at a specific index in entire history
 	public hadDeductionAtIndex(quantity: number, index: number, deduction: Deduction): boolean {
-		while (null != this.lastMove.pair) {
-			let pair = this.lastMove.pair;
+		while (null != this.lastMove.swap) {
+			let pair = this.lastMove.swap;
 			if (
 				(pair.a.quantity == quantity &&
 					pair.a.deduction == deduction &&
@@ -272,8 +320,8 @@ class Solver {
 	}
 
 	public alreadyTried(q1: number, i1: number, q2: number, i2: number): boolean {
-		while (null != this.lastMove.pair) {
-			let pair = this.lastMove.pair;
+		while (null != this.lastMove.swap) {
+			let pair = this.lastMove.swap;
 			if (
 				(pair.a.quantity == q1 &&
 					pair.a.index == i1) ||
@@ -288,8 +336,9 @@ class Solver {
 		return false;
 	}
 
-	public unswap() {
-		let move = this.newMove(this.lastMove.board, this.lastMove.pair.a.index, this.lastMove.pair.b.index);
+	public rollback() {
+		Log.out("rollback");
+		let move = new Move(this.lastMove.board, this.lastMove.swap.a.index, this.lastMove.swap.b.index);
 		// assume result is same as before
 		move.correct = this.lastMove.correct;
 		move.delta = this.lastMove.delta * -1; // assume opposite
